3afa29f6b8bc98c872e8429d44bd91c764783679
David Soto <david.soto@ridgerun.com>
2011-11-04 15:41:34 -0600

    Set 60fps by default if any framerate is set in caps allowing 60fps in RTSP

--- gstreamer_ti/ti_build/gst-openmax/omx/gstomx_base_filter.c
+++ gstreamer_ti/ti_build/gst-openmax/omx/gstomx_base_filter.c
@@ -3,6 +3,9 @@
  *
  * Author: Felipe Contreras <felipe.contreras@nokia.com>
  *
+ * Modified by: David Soto <david.soto@ridgerun.com>
+ * Copyright (C) 2011 RidgeRun
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation
